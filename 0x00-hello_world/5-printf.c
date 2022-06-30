bdaa75c73c8a905c79fa7338f15f28ddbe395c0f
fikayo <fikayodan@gmail.com>
2022-06-30 15:13:23 -0700

    c wahala

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - print another string
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char str[200];
+
+strcpy(str, "with proper grammer, but the outcome is a piece of art, \n");
+printf (str);
+ return (0);
+}
