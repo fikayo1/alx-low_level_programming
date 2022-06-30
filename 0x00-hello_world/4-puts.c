f3289ca0534e97b32eaecf1687b5340c6b704973
fikayo <fikayodan@gmail.com>
2022-06-30 14:50:36 -0700

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ *mian Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
 
 int main(void)
 {
 char str[150];
 
-strcpy(str, "Programming is like building a multilingual puzzle");
+strcpy(str, "\"Programming is like building a multilingual puzzle");
 
 puts(str);
 return (0);
-}  
+}
