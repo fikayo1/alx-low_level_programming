d6810b0a5127b320f8be1d1ac9be37af30302184
fikayo <fikayodan@gmail.com>
2022-06-30 15:52:51 -0700

    ss

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 
 /**
  * main - print sizes
- * 
+ *
  *Return: Always 0 (Success)
  */
 
@@ -15,7 +15,7 @@ long int e;
 long long int f;
 float g;
 printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of a int: %d byte(s)\n", sizeof(d));
+printf("Size of an int: %d byte(s)\n", sizeof(d));
 printf("Size of a long int: %d byte(s)\n", sizeof(e));
 printf("Size of a long long int: %d byte(s)\n", sizeof(f));
 printf("Size of a float: %d byte(s)\n", sizeof(g));
