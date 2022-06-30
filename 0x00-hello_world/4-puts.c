bcdafafcbed7fcc73abdeb2a4515906e684a5fcf
fikayo <fikayodan@gmail.com>
2022-06-30 14:40:03 -0700

    ddd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+  char str[150];
+  strcpy(str, "Programming is like building a multilingual puzzle");
+
+    puts(str);
+    return(0);
+   }  
