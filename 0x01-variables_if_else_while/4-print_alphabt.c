4a709386745eca406c9b26d2830d7d24292038f8
fikayo <fikayodan@gmail.com>
2022-07-01 19:15:04 -0700

    again

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - exceptions
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alp = 'a';
+
+while (alp <= 'z')
+{
+if (alp != 'q' && alp != 'e')
+putchar(alp);
+alp++;
+}
+}
