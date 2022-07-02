6252597b26502dc66f1c53bd7f59e5375bf7a71f
fikayo <fikayodan@gmail.com>
2022-07-01 18:23:21 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints out the alphabets in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char alp[27];
+
+  strcpy(alp, [a..z]);
+  putchar(alp);
+  return(0);
+}
