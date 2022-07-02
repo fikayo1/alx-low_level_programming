64b66f032f6ef659d5a398b7c6a11422e97c827c
fikayo <fikayodan@gmail.com>
2022-07-01 20:11:45 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - single digit
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = 0;
+ 
+while (a < 10)
+{
+putchar(a + '0');
+a++;
+putchar(',');
+putchar(' ')
+}
+
+putchar('\n');
+return (0);
+}
