a699bce79609db61bef7ddb3f9c8dc2e0e7699c0
fikayo <fikayodan@gmail.com>
2022-07-01 18:11:32 -0700

    last.digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - get the last number
  *
- *Return: Always 0 (Success) 
+ *Return: Always 0 (Success)
  */
 
 int main(void)
@@ -16,14 +16,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastdigit = n % 10;
 if (lastdigit > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n ,lastdigit);
+printf("Last digit of %d is %d and is greater than 5\n", n,lastdigit);
 else if (lastdigit == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n ,lastdigit);
+printf("Last digit of %d is %d and is 0\n", n,lastdigit);
 }
 else if (lastdigit < 6 && lastdigit != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n ,lastdigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n,lastdigit);
 }
 return (0);
 }
