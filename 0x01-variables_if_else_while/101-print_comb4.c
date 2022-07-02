16835c86a9e48759662b71da985e660cf7ab9ffe
fikayo <fikayodan@gmail.com>
2022-07-01 21:48:39 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,19 +11,19 @@ int ones;
 int tens;
 
 for (tens = '0'; tens <= '9'; tens++)
-    {
-      for (ones = (tens + 1); ones <= '9'; ones++)
-						      {
-							putchar(tens);
-							putchar(ones);
-							if (tens != '8' || ones != '9') 
-							  {
-							    putchar(',');
-							    putchar(' ');
-							  }
-						      }
-    }
-  putchar('\n');
+{
+for (ones = (tens + 1); ones <= '9'; ones++)
+{
+putchar(tens);
+putchar(ones);
+if (tens != '8' || ones != '9') 
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
 
-  return (0);
+return (0);
 }
