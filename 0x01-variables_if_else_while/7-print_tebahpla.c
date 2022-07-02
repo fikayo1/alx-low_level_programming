7b0cca714a98975eca3a9d82dab54921091e1503
fikayo <fikayodan@gmail.com>
2022-07-01 19:41:57 -0700

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the alphabets in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alp = 'z';
+
+while (alp <= 'a')
+{
+putchar(alp);
+alp--;
+}
+
+putchar('\n');
+return (0);
+}
+
