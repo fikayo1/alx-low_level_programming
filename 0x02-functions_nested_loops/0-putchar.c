3774a951c815e714fefe383293ec77b68bdca532
fikayo <fikayodan@gmail.com>
2022-07-05 01:25:40 -0700

    headers

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - print to the stdout
+ *
+ *Return: on success 0
+ */
+
+int main(void)
+{
+  _putchar(_putchar);
+  printf("\n")
+  return (0);
+}
