c72218d1059b68be1f0f60b1d6f2fde8d1294560
fikayo <fikayodan@gmail.com>
2022-07-06 03:42:35 -0700

    dub

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+/**
+ * print_alphabet - prints all th ealphabets in lower case
+ *
+ */
 void print_alphabet(void)
 {
-  int i;
-  for(i = 97; i < 123; i++)
-  _putchar(i);
+int i;
+for(i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
