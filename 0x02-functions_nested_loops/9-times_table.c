91910d7c28f2f6e64e0eb4ff457387d3827a3d60
fikayo <fikayodan@gmail.com>
2022-07-06 16:12:20 -0700

    wow

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *times_table - prints the 9 times tables, starting with 0
+ *
+ */
+
+void times_table(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i);
+_putchar(',');
+_putchar(' ');
+}
+}
