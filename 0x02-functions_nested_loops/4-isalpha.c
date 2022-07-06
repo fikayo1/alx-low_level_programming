b12151374467363f626bd3b0277dadd731576fbc
fikayo <fikayodan@gmail.com>
2022-07-06 04:37:51 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: a chacter 
+ *
+ *Return: 1 if character is an alphabet and 0 if it is not
+ */
+
+int _isalpha(int c)
+{
+if (isalpha(c) == 1)
+return (1);
+else
+return (0)
+}
