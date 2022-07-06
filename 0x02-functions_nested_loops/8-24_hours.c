cef62f379ce0c608fda96f224f3689b6ea8df35b
fikayo <fikayodan@gmail.com>
2022-07-06 06:48:58 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,6 +11,18 @@ int ht = 0;
 int hu = 0;
 int mt = 0;
 int mu = 0;
+
+ while (ht != 2 &&  hu >= 4 && mt != 0 && mu != 0)
+   {
+     if (mu > 9)
+       mu = 0;
+     if (hu > 9)
+       hu = 0;
+     if (mt > 9)
+       mt = 0;
+     for (mu = 0; i < 10; mu++)
+       _putchar(mu + '0');
+   }
 _putchar(ht + '0');
 _putchar(hu + '0');
 _putchar(':');
