48a2014e72b92655a83b6c32317206899df00bea
fikayo <fikayodan@gmail.com>
2022-07-06 04:28:50 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,15 @@
 /**
  * _islower - check if the character is lowercase
  *
+ *@c:a character
+ *
  * Return: (1) if it is lowercase and (0) if it is not
  */
 
 int _islower(int c)
 {
 if (islower(c) == 0)
-return (1);
-else
 return (0);
+else
+return (1);
 }
