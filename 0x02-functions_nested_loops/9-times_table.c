a76794a73e1889e336068bd691d52e1b0c8729d1
fikayo <fikayodan@gmail.com>
2022-07-06 16:41:17 -0700

    pu

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,8 +7,10 @@
 void times_table(void)
 {
 int i;
+int j = 0;
 
-
+while (j < 10)
+{
 for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
@@ -17,6 +19,9 @@ if (i < 9)
 _putchar(',');
 _putchar(' ');
 }
+}
 _putchar('\n');
+}
+j++;
 }
 }
