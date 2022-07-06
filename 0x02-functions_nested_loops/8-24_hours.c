1173ca84814a536238d2bb9c15d644282620c631
fikayo <fikayodan@gmail.com>
2022-07-06 06:34:32 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@
 void jack_bauer(void)
 {
 int ht = 0;
-nt  hu = 0;
+int  hu = 0;
 int mt = 0;
 int mu = 0;
 _putchar(ht + '0');
 _putchar(hu + '0');
-_putchar(hu + '0');
-_putchar(hu + '0');
+_putchar(mt + '0');
+_putchar(mu + '0');
 }
