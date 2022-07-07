b010e58d8b6b830fdad42703453a98fe157b624b
fikayo <fikayodan@gmail.com>
2022-07-07 16:40:52 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - print numbers
+ */
+
+void fizz_buzz(void)
+{
+for (i = 1; i < 101; i++)
+{
+if (i % 15 == 0)
+printf("Fizz Buzz\t");
+else if (i % 3 == 0)
+printf("Fizz\t");
+else if (i % 5 == 0)
+printf("Buzz\t");
+else
+printf("%d\t", i)
+}
+}
