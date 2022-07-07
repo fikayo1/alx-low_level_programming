783d454029fbf46f77072c69e6f85aa13a738a50
fikayo <fikayodan@gmail.com>
2022-07-07 12:49:01 -0700

    muner

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - prints the numbers from 0 to 9, without 2 and 4
+ */
+
+void print_most_numbers(void);
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+if (i != 4 || i != 2)
+_putchar(i + '0');
+}
+_putchar('\n')
+}
