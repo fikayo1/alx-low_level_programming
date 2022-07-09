804f6bc9fdb1b45e7a6a8d9c2eb96bd887dafd8f
fikayo <fikayodan@gmail.com>
2022-07-08 19:00:30 -0700

    e

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,24 +7,30 @@
 
 void print_number(int n)
 {
-int j, y;
+int  y;
  
-if (n > 9)
+if (n > 9 && n < 19)
 {
 while (n > 9)
 {
-n = n % 10;
-j++;
- 
+  n = n % 10;
 }
-_putchar(j);
+_putchar(1 + '0');
 _putchar(n + '0');
 }
 else if (n < 0)
 {
 y = n * -1;
 _putchar('-');
-_putchar(y + '0');
+if (y > 9)
+{
+while (y > 9)
+{
+n = y % 10;
+}
+_putchar(9 + '0');
+_putchar(n + '0');
+}
 }
 else
 {
