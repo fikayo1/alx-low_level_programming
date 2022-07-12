6600e31980a007bfdd258eb506059637ddf60ec3
fikayo <fikayodan@gmail.com>
2022-07-11 18:48:21 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all the multiples of 3 and five in 1024
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  int i;
+  int sum;
+  
+  for (i = 0; i < 1024; i++)
+    {
+      if (i % 3 == 0 || i % 5 == 0)
+	{
+	  sum += i;
+	}
+    }
+  printf("%d", sum);
+  return (0);
+}
