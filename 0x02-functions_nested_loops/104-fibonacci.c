0a23159d3c8edefc7c68e74b008d1f2bd12eb5e5
fikayo <fikayodan@gmail.com>
2022-07-11 19:10:42 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,24 @@
 
 int main(void)
 {
-  int i;
-  int c = 1;
-  int k = 2;
-  int j;
+int i;
+unsigned long int c = 1;
+unsigned long int k = 2;
+unsigned long int j;
 
-  printf("%d, ", c);
-  printf("%d, ", k);
-  for (i = 1; i <= 98; i++)
-    {
-      j = c + k;
-      c = k;
-      k = j;
-	  printf("%d, ", j);
-    }
-  printf("\n");
-  return (0);
+printf("%d, ", c);
+printf("%d, ", k);
+for (i = 1; i <= 98; i++)
+{
+j = c + k;
+c = k;
+k = j;
+printf("%d", j);
+if (1 < 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
 }
