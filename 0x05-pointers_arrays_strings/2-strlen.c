6a2359189393064f7ff1f8e4d6ae25df0e447721
fikayo <fikayodan@gmail.com>
2022-07-12 14:08:23 -0700

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+int i;
+
+while (s[i] != '\0')
+{
+u++;
+}
+return (u);
+}
