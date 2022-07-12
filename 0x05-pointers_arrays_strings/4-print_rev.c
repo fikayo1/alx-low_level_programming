e6e715e475187b79c14937f3c656f1808ca21358
fikayo <fikayodan@gmail.com>
2022-07-12 14:55:37 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: a pointer to a string
+ */
+void print_rev(char *s)
+{
+int i;
+int sizearr;
+
+sizearr = sizeof(s) / sizeof(arr[0]);
+for (i = sizearr; i > 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
