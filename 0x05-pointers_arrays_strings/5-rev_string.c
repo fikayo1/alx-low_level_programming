2fa095c88809b3c65d39106ea9fd6d3bdc6177b9
fikayo <fikayodan@gmail.com>
2022-07-12 15:58:22 -0700

    print string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,19 +5,29 @@
  */
 void rev_string(char *s)
 {
-int i;
-int a = 0;
-char arr[100];
+char *start_c, *end_c, c;
+int i, count;
+int length = 0;
 
-while (s[a] != '\0')
+for (i = 0; s[i]; i++)
 {
-a++;
+length++;
 }
-a -= 1;
-for (i = a; i >= 0; i--)
+count = length;
+start_c = s;
+end_c = s;
+
+for (i = 0; i < count - 1; i++)
 {
-arr[i - (i - 1)] = s[i];
-
-*s = arr;
+end_c++;
 }
+for (i = 0; i < count / 2; i++)
+{
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
 
+start_c++;
+end_c--;
+}
+}
