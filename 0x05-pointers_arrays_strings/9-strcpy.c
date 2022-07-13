9793a8417beea6a4373e1f46dc3e25071b4ea452
fikayo <fikayodan@gmail.com>
2022-07-12 17:39:30 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string pointed to scr
+ * @scr: source string
+ * @dest: a pointer pointing to destination address
+ * Return: the pointer to dest
+ *
+ */
+char *_strcpy(char *dest, char *scr)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+
+}
