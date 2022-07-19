3f38ac02c172449b970e4af06efa351120a3654a
fikayo <fikayodan@gmail.com>
2022-07-18 18:33:56 -0700

    copy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
