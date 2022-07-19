146b0cd1511689cd2490123f2647e178ef084f24
fikayo <fikayodan@gmail.com>
2022-07-18 19:25:05 -0700

    resr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,8 +2,8 @@
 /**
  * _strchr - locates a character in a string
  * @s: a pointer to the string
- * @n: a character to be checked for
- * Return: a pointer to the first occurrence of the c in the s, or NULL if the character is not found
+ * @c: a character to be checked for
+ * Return: a pointer to the first occurrence of the c in the s
  */
 char *_strchr(char *s, char c)
 {
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strspn - gets the length of a prefix substring
- * @s: 
- * @accept:
+ * @s:This the main C string to bw scanned
+ * @accept:This is the string containing thet list of characters to match in s
  * Return: number of bytes in the initial segment of s
  */
 unsigned int _strspn(char *s, char *accept)
