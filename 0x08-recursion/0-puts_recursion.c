caeb715c0635d5f3f75f4a95c0bc1e4484a4bcfc
fikayo <fikayodan@gmail.com>
2022-07-20 18:01:25 -0700

    w

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string 
+ * _puts_recursion - prints a string
  * @s: the memory locatio contain to be printed
  */
 void _puts_recursion(char *s)
