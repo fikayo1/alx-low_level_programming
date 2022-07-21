1e774b844846bb4012b8118dc8659c7f01ae21f6
fikayo <fikayodan@gmail.com>
2022-07-20 21:25:53 -0700

    omoo

---
+++ 0x08-recursion/100-ispalindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_palindrome - checkes if a string is a palindrome
+ * @s: a pointer to the string to be checked
+ * Return: 1 if it a palindrome and 0 if it isn't
+ */
+int is_palindrome(char *s)
+{
+if (s == ' ')
+{
+return (1);
+}
+
+}
