1670ad8ccb1db671babace67f9f8a5950da195c3
fikayo <fikayodan@gmail.com>
2022-07-20 17:41:36 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: a pointer to the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_puts_recursion(s + 1);
+_putchar(*s);
+}
+_putchar('\n');
+}
