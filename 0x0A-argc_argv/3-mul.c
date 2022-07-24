f356e301df09c904e4e3c89e95f6831d6e34fcda
fikayo <fikayodan@gmail.com>
2022-07-23 19:25:12 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints its name followed by a new line
+ * @argc: the numbers of arguments
+ * @argv: the argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+if (argc == 2)
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
+printf("Error");
+return (1);
+}
