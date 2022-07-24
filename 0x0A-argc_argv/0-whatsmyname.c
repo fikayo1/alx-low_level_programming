1fbe6c6649e03be6b4335783593d410eb84f06bd
fikayo <fikayodan@gmail.com>
2022-07-23 18:57:29 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name followed by a new line
+ * @argc: the numbers of arguments
+ * @:argv: the argument vector
+ * Return: 0 on success
+ */
+int main(argc, *argv[])
+{
+printf(argv[0]);
+printf("\n");
+}
