33a0a7e11cb52eb7b3f2797dbd36842e547c2165
fikayo <fikayodan@gmail.com>
2022-07-23 19:04:17 -0700

    unused variables

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @:argv: the argument vector
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
