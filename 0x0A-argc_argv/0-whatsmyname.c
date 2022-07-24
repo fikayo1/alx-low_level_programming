6b35b9fc47a00abf0dfa623672d471c5e736ea57
fikayo <fikayodan@gmail.com>
2022-07-23 19:05:41 -0700

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 /**
  * main - prints its name followed by a new line
  * @argc: the numbers of arguments
- * @:argv: the argument vector
+ * @argv: the argument vector
  * Return: 0 on success
  */
 int main(int __attribute__ ((unused)) argc, char *argv[])
