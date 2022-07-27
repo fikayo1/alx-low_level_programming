76adeced2eb4e5ba0c3d52e25b1163ce5e6763c2
fikayo <fikayodan@gmail.com>
2022-07-26 19:48:01 -0700

    try

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - copies a string to another memory location
+ * @str: string to be copied
+ * Return: a pointer to thet new string, null if string is null
+ */
+char *_strdup(char *str)
+{
+char *ns;
+int i = 0;
+int j;
+
+if (str == NULL)
+{
+return (NULL);
+}
+while (str[i] != '\0')
+{
+i++;
+}
+ns = malloc(i * sizeof(char));
+if (ns == NULL)
+return (NULL);
+for (j = 0; j < i; j++)
+{
+ns[j] = str[j];
+}
+ns[j] = '\0';
+return (ns);
+}
