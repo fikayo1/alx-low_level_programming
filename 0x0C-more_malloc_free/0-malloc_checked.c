72f3f2f96be5fe1848dec2458275f8ce34a10fdd
fikayo <fikayodan@gmail.com>
2022-07-28 13:25:22 -0700

    initial commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: an integer
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+int *ar;
+
+ar = malloc(b);
+{
+if (ar == NULL)
+{
+free(ar);
+return (98);
+}
+return (ar);
+}
+}
