cc8fd1f13adbb4279b978f021fe16ffdc4599dda
fikayo <fikayodan@gmail.com>
2022-08-01 13:58:04 -0700

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from
+ * @argc: the number of elements it takes in
+ * @argv: argument vector
+ * Return: o on success
+ */
+int main(__attribute__((unused)) argc, *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
