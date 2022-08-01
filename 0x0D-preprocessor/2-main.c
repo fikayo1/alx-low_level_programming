939ea026dd5ae5b991e2f880ad7a9eb47e621d17
fikayo <fikayodan@gmail.com>
2022-08-01 14:00:13 -0700

    print file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,7 +5,7 @@
  * @argv: argument vector
  * Return: o on success
  */
-int main(__attribute__((unused)) argc, *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
