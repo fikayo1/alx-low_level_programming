178b6f4051217ea0cea9abea842f847b50d0283d
fikayo <fikayodan@gmail.com>
2022-08-01 01:09:04 -0700

    description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - a struct with some elements in it
+ * @name: a pointer to the name string
+ * @age: a pointer to age
+ * @owner: a poiner to owner
+ */
 struct dog
 {
 char *name;
