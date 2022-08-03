70156866eb9ba7166ea64c305ea9413a5520e47b
fikayo <fikayodan@gmail.com>
2022-08-03 07:46:57 -0700

    sd

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-long unsigned int i;
- 
+unsigned int i;
+
 if (!array && !action)
 {
-return ;
+return;
 }
 for (i = 0; i < size; i++)
 {
