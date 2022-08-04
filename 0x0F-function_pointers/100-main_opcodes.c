9d82e3086aa3367c76c0fb4482ff14d0eb7e53e4
fikayo1 <soetandan@gmail.com>
2022-08-04 01:52:20 +0100

    previous

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,12 +15,12 @@ if (argc != 2)
 printf("Error\n");
 exit(1);
 }
-if (argv[1] < 0)
+n1 = atoi(argv[1])
+if (n1 < 0)
 {
 printf("Error\n");
 exit(2);
 }
-n1 = atoi(argv[1]);
 for (count = 0; count < n1; count++)
 {
 printf("%02hhx", *((char *)main + count));
