95484439d0156115925415ad3052e668653e15c9
fikayo1 <soetandan@gmail.com>
2022-08-16 09:31:37 +0100

    pop elements

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: the first node of a linked list
+ * Return: the head node data
+ */
+int pop_listint(listint_t **head)
+{
+int h;
+listint_t *temp;
+listint_t *curr;
+
+curr = *head;
+h = curr->n;
+temp = curr->next;
+
+free(curr);
+*head = temp;
+return (h);
+}
